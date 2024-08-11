1254a71135019b5d944276a6cb9cb6a027c6e173
888KEKC888 <sitneckov@mail.ru>
2024-08-11 15:02:54 +0300

    change test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,6 +1,7 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -8,10 +9,9 @@ class PostmanEchoTest {
 
     @Test
     void shouldReturnSendData() {
-        String text = "приветики";
+        String text = "Hi my name is";
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
                 .body(text)
                 .when()
                 .post("/post")
